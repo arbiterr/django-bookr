8743cf2ab792451faf30db9c3a7e337fa9d14f3b
Attila Lörinc <arbiter_la@hotmail.com>
2020-02-13 23:15:23 +0100

    list 5 most recently added books on the dashboard

--- books/tests.py
+++ books/tests.py
@@ -4,7 +4,12 @@ from django.urls import reverse
 from .models import Author, Book
 
 
-class DashboardTests(TestCase):
+class DashboardViewEmptyDBTests(TestCase):
+    '''Test dashboard without fixtures'''
+
+    def setUp(self):
+        self.author = Author.objects.create(
+            first_name="J. D.", last_name="Salinger")
 
     def test_url(self):
         response = self.client.get('/')
@@ -17,6 +22,29 @@ class DashboardTests(TestCase):
     def test_template_used(self):
         response = self.client.get(reverse('books:dashboard'))
         self.assertTemplateUsed(response, template_name='books/index.html')
+
+    def test_no_books(self):
+        response = self.client.get(reverse('books:dashboard'))
+        self.assertContains(response, 'No books rated yet.')
+        self.assertContains(response, 'No books read yet.')
+        self.assertContains(response, 'No books added yet.')
+
+
+class DashboardViewSmallDBTests(TestCase):
+    '''Test dashboard with fixtures with a few data'''
+
+    fixtures = ['threebooks.json']
+
+    def test_recently_added_books(self):
+        response = self.client.get(reverse('books:dashboard'))
+        self.assertQuerysetEqual(
+            response.context['recent_books'],
+            [
+                repr(Book.objects.get(pk=3)),
+                repr(Book.objects.get(pk=2)),
+                repr(Book.objects.get(pk=1))
+            ]
+        )
 
 
 class AuthorModelTests(TestCase):
--- books/views.py
+++ books/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from .models import Book
+
 
 def dashboard(request: HttpRequest) -> HttpResponse:
-    return render(request, 'books/index.html', {})
+    ctx = {
+        "recent_books": Book.objects.order_by('-added')[:5]
+    }
+    return render(request, 'books/index.html', ctx)
